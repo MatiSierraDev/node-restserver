d88722109eb8edcb308646bf9e592405a468c0b9
Matias Sierra <matisierra.develop@gmail.com>
2021-11-22 14:30:02 -0300

    fixed db connection

--- database/config.js
+++ database/config.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
 
   try {
-    await mongoose.connect(process.env.MONGODB_CNN)
-    .then(res => console.log('Conectado a la base de datos!'));
+    await mongoose
+      .connect(process.env.MONGODB_CNN, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      })
+      .then((res) => console.log("Conectado a la base de datos!"));
   
   } catch (error) {
     throw new Error('Error a la hora de inicializar el proceso')
