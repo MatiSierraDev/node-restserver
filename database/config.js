81d279dfafdbb7559893cdf037c07eda6a876072
Matias Sierra <matisierra.develop@gmail.com>
2021-11-23 22:54:46 -0300

    fix connect db

--- database/config.js
+++ database/config.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
 
   try {
-    await mongoose.connect(process.env.MONGODB_CNN)
-    .then(res => console.log('Conectado a la base de datos!'));
+    await mongoose.connect(
+      process.env.MONGODB_CNN,
+      {},
+      () => {console.log('Conectado a la base de datos!')}
+      )
   
   } catch (error) {
     throw new Error(error)
