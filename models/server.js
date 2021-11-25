ade6292eb84822e856a4ec747535faa6d31fbe0a
Matias Sierra <matisierra.develop@gmail.com>
2021-11-24 21:53:47 -0300

    Add auth-login path to server.js

--- models/server.js
+++ models/server.js
@@ -6,7 +6,9 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 3000;
+    
     this.api = '/api/users';
+    this.auth = '/auth';
     
     //db connection
     this.getDb();
@@ -35,6 +37,8 @@ class Server {
   }
 
   routes(){
+
+    this.app.use(this.auth, require('../routes/auth'))
     this.app.use(this.api, require( '../routes/users' ));
 
     this.app.get('*', (req, res) => {
